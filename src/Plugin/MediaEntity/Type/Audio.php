95ad306d33e238d5f67f290e12d44891a09e6229
Arit Kumar Nath <arit.nath@dcplkolkata.com>
2016-07-05 20:20:12 +0530

    Update Audio.php

--- src/Plugin/MediaEntity/Type/Audio.php
+++ src/Plugin/MediaEntity/Type/Audio.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\media_entity_audio\Plugin\MediaEntity\Type;
 
-use Drupal\media_entity\MediaBundleInterface;
 use Drupal\media_entity\MediaInterface;
 use Drupal\media_entity\MediaTypeBase;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides media type plugin for Audio.
@@ -17,29 +19,44 @@ use Drupal\media_entity\MediaTypeBase;
  */
 class Audio extends MediaTypeBase {
 
+  
   /**
    * {@inheritdoc}
    */
-  public function settingsForm(MediaBundleInterface $bundle) {
-    $form = array();
-
-    $options = array();
-    $allowed_field_types = array('file');
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    /** @var \Drupal\media_entity\MediaBundleInterface $bundle */
+    $bundle = $form_state->getFormObject()->getEntity();
+    $options = [];
+    $allowed_field_types = ['file', 'image'];
     foreach ($this->entityFieldManager->getFieldDefinitions('media', $bundle->id()) as $field_name => $field) {
       if (in_array($field->getType(), $allowed_field_types) && !$field->getFieldStorageDefinition()->isBaseField()) {
         $options[$field_name] = $field->getLabel();
       }
     }
-    $form['source_field'] = array(
+
+    $form['source_field'] = [
       '#type' => 'select',
       '#title' => t('Field with source information'),
-      '#description' => t('Field on media entity that stores Audio file.'),
+      '#description' => t('Field on media entity that stores Image file. You can create a bundle without selecting a value for this dropdown initially. This dropdown can be populated after adding fields to the bundle.'),
       '#default_value' => empty($this->configuration['source_field']) ? NULL : $this->configuration['source_field'],
       '#options' => $options,
-    );
+    ];
+
+    $form['gather_exif'] = [
+      '#type' => 'select',
+      '#title' => t('Whether to gather exif data.'),
+      '#description' => t('Gather exif data using exif_read_data().'),
+      '#default_value' => empty($this->configuration['gather_exif']) || !function_exists('exif_read_data') ? 0 : $this->configuration['gather_exif'],
+      '#options' => [
+        0 => t('No'),
+        1 => t('Yes'),
+      ],
+      '#disabled' => (function_exists('exif_read_data')) ? FALSE : TRUE,
+    ];
 
     return $form;
   }
+
 
   /**
    * {@inheritdoc}
