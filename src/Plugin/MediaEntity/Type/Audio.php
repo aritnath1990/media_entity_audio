f290dbff03b068c6099f03c8b14cf0c939749767
Arit Kumar Nath <arit.nath@dcplkolkata.com>
2016-07-05 20:45:53 +0530

    update

--- src/Plugin/MediaEntity/Type/Audio.php
+++ src/Plugin/MediaEntity/Type/Audio.php
@@ -4,8 +4,6 @@ namespace Drupal\media_entity_audio\Plugin\MediaEntity\Type;
 
 use Drupal\media_entity\MediaInterface;
 use Drupal\media_entity\MediaTypeBase;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -19,7 +17,6 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class Audio extends MediaTypeBase {
 
-  
   /**
    * {@inheritdoc}
    */
@@ -27,7 +24,7 @@ class Audio extends MediaTypeBase {
     /** @var \Drupal\media_entity\MediaBundleInterface $bundle */
     $bundle = $form_state->getFormObject()->getEntity();
     $options = [];
-    $allowed_field_types = ['file', 'image'];
+    $allowed_field_types = ['file'];
     foreach ($this->entityFieldManager->getFieldDefinitions('media', $bundle->id()) as $field_name => $field) {
       if (in_array($field->getType(), $allowed_field_types) && !$field->getFieldStorageDefinition()->isBaseField()) {
         $options[$field_name] = $field->getLabel();
@@ -37,26 +34,13 @@ class Audio extends MediaTypeBase {
     $form['source_field'] = [
       '#type' => 'select',
       '#title' => t('Field with source information'),
-      '#description' => t('Field on media entity that stores Image file. You can create a bundle without selecting a value for this dropdown initially. This dropdown can be populated after adding fields to the bundle.'),
+      '#description' => t('Field on media entity that stores Audio file. You can create a bundle without selecting a value for this dropdown initially. This dropdown can be populated after adding fields to the bundle.'),
       '#default_value' => empty($this->configuration['source_field']) ? NULL : $this->configuration['source_field'],
       '#options' => $options,
     ];
 
-    $form['gather_exif'] = [
-      '#type' => 'select',
-      '#title' => t('Whether to gather exif data.'),
-      '#description' => t('Gather exif data using exif_read_data().'),
-      '#default_value' => empty($this->configuration['gather_exif']) || !function_exists('exif_read_data') ? 0 : $this->configuration['gather_exif'],
-      '#options' => [
-        0 => t('No'),
-        1 => t('Yes'),
-      ],
-      '#disabled' => (function_exists('exif_read_data')) ? FALSE : TRUE,
-    ];
-
     return $form;
   }
-
 
   /**
    * {@inheritdoc}
@@ -68,7 +52,7 @@ class Audio extends MediaTypeBase {
   /**
    * {@inheritdoc}
    */
-  public function getField(MediaInterface $media , $name) {
+  public function getField(MediaInterface $media, $name) {
     return FALSE;
   }
 
